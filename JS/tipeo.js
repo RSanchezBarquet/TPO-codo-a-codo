4b4fac05a5f5762fd5102366c77cf7845f489511
Romina Sanchez Barquet <rsanchezbarquet@gmail.com>
2022-06-24 13:29:04 -0300

    tpo

---
+++ JS/tipeo.js
@@ -0,0 +1,17 @@
+var text = document.getElementById('titulo');
+var str = text.innerHTML;
+
+text.innerHTML = '';
+
+var speed =200;
+var i=0;
+
+function typeWriter(){
+    if(i<str.length){
+        text.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+setTimeout(typeWriter, speed);
